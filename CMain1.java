39c782a19001be5cb4fbd2bfe37656d878ca8c91
Riseh4rd <112888563+Riseh4rd@users.noreply.github.com>
2022-09-26 14:29:12 +0300

    Create CMain1.java

---
+++ CMain1.java
@@ -0,0 +1,11 @@
+public class Control {
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+        Gui gui = new Gui(calculator);
+        gui.setVisible(true);
+        boolean exit = false;
+        while(!exit) {
+
+        }
+    }
+}
